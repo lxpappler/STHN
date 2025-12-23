03655516ff6f774c913e4b6e078753be0f7ccacc
liuxp <liuxp22@mails.tsinghua.edu.cn>
2025-12-23 13:29:03 +0800

    Debug flowhist plot

--- local_pipeline/plot_hist.py
+++ local_pipeline/plot_hist.py
@@ -15,8 +15,11 @@ def plot_hist_helper(path):
 
     plt.figure()
     flow_data = np.load(f'{path}/flownpy.npy', allow_pickle=True)
-    n, bins, patches = plt.hist(x=flow_data, bins=np.linspace(0, 100, 20))
-    plt.title("Test Flow")
+    max_val = np.max(flow_data)
+    n, bins, patches = plt.hist(x=flow_data, bins=np.linspace(0, max_val, 50))
+    plt.title(f"Test Flow (Max: {max_val:.2f})")
+    # n, bins, patches = plt.hist(x=flow_data, bins=np.linspace(0, 100, 20))
+    # plt.title("Test Flow")
     plt.ylim(0, 20000)
     plt.xlabel("Flow")
     plt.ylabel("Frequency")
@@ -24,5 +27,5 @@ def plot_hist_helper(path):
     plt.close()
 
 if __name__ == '__main__':
-    path = "IHN_results/satellite_thermal_dense"
+    path = "/home/liuxp/Documents/Projects/STHN_clean/test/local_he/1536_one_stage/satellite_0_thermalmapping_135-2025-12-22_07-07-33"
     plot_hist_helper(path)
